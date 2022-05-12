6406cd39c206beddfe326f24c5741a5ac6fb5981
Jlexand <zlexandz@gmail.com>
2022-05-12 20:16:50 +0300

    Базовые функции

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <button v-on:click="addLike">Like</button>
+    <button @click="addDislike">dislikes</button>
+</div>
+    <div>
+        кол-во лайков: <strong>{{likes}}</strong>
+    </div>
+    <div>
+        Кол-во дизлайков: <strong>{{dislikes}}</strong>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                likes: 0,
+                dislikes: 4,
+            }
+        },
+         methods: {
+             addLike() {
+                 this.likes += 1;
+             },
+             addDislike() {
+                 this.dislikes += 1;
+             }
+         },
+    }
+</script>
