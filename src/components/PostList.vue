a046eebb10a16b6a234cf2267fd084bf521b7bd3
Jlexand <zlexandz@gmail.com>
2022-06-28 09:06:10 +0300

    Поиск постов. Фильтрация

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <h4>Список пользователей</h4>
-    <PostItem
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-      v-for="post in posts"
-    />
+    <transition-group name="post-list">
+      <PostItem
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+        v-for="post in posts"
+      />
+    </transition-group>
   </div>
 </template>
 
@@ -24,3 +26,21 @@ export default {
   },
 };
 </script>
+<style>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(80px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
